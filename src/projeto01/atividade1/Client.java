982d1d5f05e7ff97bca5d5deb3f4257f76c61f0a
Aline Marques <aline.marqs@gmail.com>
2021-03-10 21:10:03 -0300

    Initial commit

---
+++ src/projeto01/atividade1/Client.java
@@ -0,0 +1,48 @@
+/* Author: Aline Marques da Silva */
+
+package projeto01.atividade1;
+
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.InputStreamReader;
+import java.net.Socket;
+
+public class Client {
+    
+    private static Socket socket;
+    private static DataInputStream entrada;
+    private static DataOutputStream saida;
+    
+    public static void main(String[] args) {
+        
+        try {
+            
+            socket = new Socket("127.0.0.1", 50000);
+            
+            entrada = new DataInputStream(socket.getInputStream());
+            saida = new DataOutputStream(socket.getOutputStream());
+            
+            //Recebe do usuário algum valor
+            BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+            System.out.println("Digite o número do cpf (sem pontuações) : ");
+            String cpf = br.readLine();
+            
+            //O valor é enviado ao servidor
+            saida.writeUTF(cpf);
+            
+            //Recebe-se o resultado do servidor
+            boolean resultado = entrada.readBoolean();
+            
+            //Mostra o resultado na tela
+            System.out.println(resultado);
+            
+            socket.close();
+            
+        } catch(Exception e) {
+            
+        }
+        
+    }
+    
+}
---
+++ src/projeto01/atividade1/Server.java
@@ -0,0 +1,52 @@
+/* Author: Aline Marques da Silva */
+
+package projeto01.atividade1;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    
+    private static Socket socket;
+    private static ServerSocket server;
+    
+    private static DataInputStream entrada;
+    private static DataOutputStream saida;
+
+    public static void main(String[] args) {
+        
+        try {
+        
+            //Criar porta de recepção
+            server = new ServerSocket(50000);
+            socket = server.accept();
+    
+            //Criar os fluxos de entrada e saída
+            entrada = new DataInputStream(socket.getInputStream());
+            saida = new DataOutputStream(socket.getOutputStream());
+            
+            //Recebimento do valor
+            String cpf = entrada.readUTF();
+            System.out.println(cpf);
+
+            boolean cpfvalido;
+
+            //Validação e Processamento do valor
+            if (cpf.isBlank() || cpf.isEmpty()) {
+                cpfvalido = false;
+            } else {
+                cpfvalido = ValidarCpf.isCPF(cpf);
+            }
+
+            //Envio dos dados (resultado)
+            saida.writeBoolean(cpfvalido);
+
+            socket.close();
+            
+        } catch(Exception e) {
+            
+        }
+    }
+}
